a821d67eb501f073ddfdd54627708bbc8b243eed
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-27 12:57:18 +0100

    Update register.php

--- register.php
+++ register.php
@@ -12,10 +12,10 @@
 	<div id="navBar">
 	<h2>LifeStream</h2>
 	<ul>
-	<li><a href="../logout/logout.php">Logout</a></li>
-	<li><a href="../feedback/feedback.php">FeedBack</a></li>
+	<li><a href="logout.php">Logout</a></li>
+	<li><a href="feedback.php">FeedBack</a></li>
 	<li><a href="../why_donate_blood/wdb.php">Why Donate Blood</a></li>
-	<li><a href="../home/home.php">Home</a></li>
+	<li><a href="home.php">Home</a></li>
 	</ul>
 	</div>
 	<?php
@@ -32,9 +32,9 @@
 <div id="navBar">
 <h2>LifeStream</h2>
 <ul>
-  <li><a href="../feedback/feedback.php">FeedBack</a></li>
-  <li><a href="../why_donate_blood/wdb.php">Why Donate Blood</a></li>
-  <li><a href="../home/home.php">Home</a></li>
+  <li><a href="feedback.php">FeedBack</a></li>
+  <li><a href="why_donate_blood/wdb.php">Why Donate Blood</a></li>
+  <li><a href="home.php">Home</a></li>
 </ul>
 </div>
   <?php

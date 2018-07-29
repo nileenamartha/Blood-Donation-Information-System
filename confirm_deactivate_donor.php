7b7a178043fe96ee501068520aa2c49b931004bd
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-29 16:17:16 +0100

    Add files via upload

---
+++ confirm_deactivate_donor.php
@@ -0,0 +1,40 @@
+<?php
+ session_start();
+	  $lid=$_SESSION["l_id"];
+ if(!isset($_SESSION['l_id']))
+	  {
+      header("Location:index.php"); 
+	  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>LifeStream Blood Donation</title>
+<link rel="stylesheet" href="styleconfirmdeactivate.css">
+</head>
+<body>
+<div id="navBar">
+<h2>LifeStream</h2>
+<ul>
+ <li><a href="logout.php">Logout</a></li>
+  <li><a href="login.php">MyProfile</a></li>
+  <li><a href="index.php">Home</a></li>
+</ul>
+</div>
+  <body>
+<section class="homeTop" id="top">
+<div id="text">
+<p>
+<span style="font-family:Lucida Calligraphy;">Are you sure you want to deactivate the profile?.</span>
+</p>
+<blockquote>
+  <blockquote>
+    <p>
+      <button onClick="window.location.href='action_deactivate.php'">Yes</button><npbs><npbs> 
+      <button onClick="window.location.href='login.php'">No</button>
+    </p>
+  </blockquote>
+</blockquote>
+</div>
+</body>
+</html>

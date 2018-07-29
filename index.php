192e156a89d9e6ac2a1fac5bb711160448edb598
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-29 15:38:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<html>
 	<head>
 	<title>LifeStream Blood Donation</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="styleindex.css">
 	</head>
 	<body>
 	<div id="navBar">
@@ -28,7 +28,7 @@
 <html>
 <head>
 <title>LifeStream Blood Donation</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="styleindex.css">
 </head>
 <body>
 <div id="navBar">

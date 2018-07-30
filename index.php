b61fdd10ca18318bd5688790b578b68b073be9f7
AnjithaJohnson <39301324+AnjithaJohnson@users.noreply.github.com>
2018-07-30 21:10:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	<li><a href="logout.php">Logout</a></li>
 	<li><a href="feedback.php">FeedBack</a></li>
 	<li><a href="wdb.php">Why Donate Blood</a></li>
-	<li><a href=>Home</a></li>
+	<li><a href="index.php">Home</a></li>
 	</ul>
 	</div>
 	<?php

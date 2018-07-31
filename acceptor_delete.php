6408aa6baab08b4ad34191543860a1f6113c2db5
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-31 14:48:32 +0100

    Update acceptor_delete.php

--- acceptor_delete.php
+++ acceptor_delete.php
@@ -1,6 +1,6 @@
 <?php
 $temp=$_GET["na"];
-$conn = mysqli_connect("localhost", "root", "","blood_info");
+$conn = mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
 $sql="delete from login where id='$temp'";
 $result=mysqli_query($conn,$sql);
 $sql="delete from acceptor_register where l_id='$temp'";

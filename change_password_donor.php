3ada969c8465ef3e3fd19d1394b95a5f39c0ed00
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-31 13:36:13 +0100

    Update change_password_donor.php

--- change_password_donor.php
+++ change_password_donor.php
@@ -38,7 +38,7 @@
 	  $current=$_POST["current"];
 	  $new=$_POST["new"];
 	  $confirm=$_POST["confirm"];
-	  $conn = mysqli_connect("localhost", "root","","blood_info");
+	  $conn = mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
 	  $sql="select password from login where id='$lid'";
 	  $result=mysqli_query($conn,$sql);
 	  if(mysqli_num_rows($result)>0)

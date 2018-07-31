1b9f4d31ecee5eed3137967f4cfccc128dbb5ae0
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-31 13:17:29 +0100

    Update action_deactivate.php

--- action_deactivate.php
+++ action_deactivate.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $lid=$_SESSION["l_id"];
-$conn = mysqli_connect("localhost", "root", "","blood_info");
+$conn = mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
 $sql="delete from donor_register where l_id='$lid'";
 $result=mysqli_query($conn,$sql);
 $sql="delete from login where id='$lid'";

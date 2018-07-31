5fd5216ad6645dc857c491ec577610c63b0e62e4
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-31 13:16:28 +0100

    Update action_delete.php

--- action_delete.php
+++ action_delete.php
@@ -1,6 +1,6 @@
 <?php
 $id=$_GET["na"];
-$conn=mysqli_connect("localhost","root","","blood_info");
+$conn=mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
 $sql="delete from notification where id='$id'";
 $result=mysqli_query($conn,$sql);
 if($result)

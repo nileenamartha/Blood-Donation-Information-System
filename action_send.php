c6eb20ae5e7c7c93c5987178255ff8144e94ea6c
Nileena Martha Vincent <10385521@mydbs.ie>
2018-07-31 13:19:27 +0100

    Update action_send.php

--- action_send.php
+++ action_send.php
@@ -3,7 +3,7 @@ $name=$_POST["name"];
 $email=$_POST["email"];
 $location=$_POST["location"];
 $suggestion=$_POST["suggestion"];
-$conn=mysqli_connect("localhost","root","","blood_info");
+$conn=mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
 $sql="insert into feedback values(null,CURDATE(),'$name','$email','$location','$suggestion')";
 $result=mysqli_query($conn,$sql);
 if($result)

47b16d0f0258810b4d2b6fc4b94f3f7d113d29ed
AnjithaJohnson <39301324+AnjithaJohnson@users.noreply.github.com>
2018-08-03 00:21:43 +0100

    Create action_reject.php

---
+++ action_reject.php
@@ -0,0 +1,59 @@
+<?php
+	  $conn=mysqli_connect("localhost","root","","blood_info");
+      session_start();
+      $lid=$_SESSION["l_id"];
+	  $nid=$_GET["na"];
+	  $sql="select status from notification_donor_info where l_id='$lid' and n_id='$nid'";
+	  $result=mysqli_query($conn,$sql);
+	  if(mysqli_num_rows($result)>0)
+		{
+			while($data=mysqli_fetch_array($result))
+			{
+				$status=$data['status'];
+				if($status=='rejected')
+				{
+					?>
+					<script>
+					alert("Already rejected");
+					window.location="notification.php";
+					</script>
+					<?php
+				}
+			}
+		}
+		
+		else
+		{
+	  $sql="select * from donor_register where l_id='$lid'";
+	  $result=mysqli_query($conn,$sql);
+	  if(mysqli_num_rows($result))
+		{
+			while($data=mysqli_fetch_array($result))
+			{
+				$name=$data['name'];
+				$bloodgroup=$data['blood_group'];
+				$phone_no=$data['phone_no'];
+				$email=$data['email'];
+				$gender=$data['gender'];
+				$state=$data['state'];
+				$district=$data['district'];
+				$location=$data['location'];
+				
+				$sql1="insert into notification_donor_info values(null,'$lid','$nid','$name','$bloodgroup','$phone_no','$email','$gender','$state','$district','$location',curdate(),'rejected')";
+		$result1=mysqli_query($conn,$sql1);
+			}
+		}
+		  if($result1)
+			{
+				$sql2="update notification set status='accepted' where id='$nid'";
+				$result2=mysqli_query($conn,$sql2);
+				?>
+				<script>
+				alert("Request successfully rejected");
+				window.location="notification.php"
+				</script>
+				<?php
+			}
+		}
+		
+?>

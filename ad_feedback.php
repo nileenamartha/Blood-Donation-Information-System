ff2773cd0eb0aa275d246d1183cffc97220c9fd0
AnjithaJohnson <39301324+AnjithaJohnson@users.noreply.github.com>
2018-08-03 12:51:50 +0100

    Update ad_feedback.php

--- ad_feedback.php
+++ ad_feedback.php
@@ -34,7 +34,7 @@
   </tr>
    <?php
    $i=1;
-  $conn=mysqli_connect("localhost","root","","blood_info");
+  $conn=mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
   $sql="select * from feedback";
   $result=mysqli_query($conn,$sql);
   if(mysqli_num_rows($result)>0)

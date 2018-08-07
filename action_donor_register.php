54869966104aeec9965563252b0edaf45c671105
Nileena Martha Vincent <10385521@mydbs.ie>
2018-08-07 13:55:42 +0100

    Update action_donor_register.php

--- action_donor_register.php
+++ action_donor_register.php
@@ -25,7 +25,8 @@ if($pwd!=$cpwd)
 }
 else
 {
-    $conn = mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6") $sql="INSERT INTO login VALUES (null,'$email','$pwd','pending','$phone_no')";
+    $conn = mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
+	$sql="INSERT INTO login VALUES (null,'$email','$pwd','pending','$phone_no')";
 	$result=mysqli_query($conn,$sql);
 	if(!$result)
 	{
@@ -47,30 +48,6 @@ else
 			alert("Registration Completed.");
 			window.location.href="index.php";
 		</script>
-
-	<?php
-		//Testing SmS
-
-	// Authorisation details.
-	$username = "nileenamv@gmail.com";
-	$hash = "bdbb469355e2139b95b6e545bee29c6d4027d7e19d482f0782aed18fe75350f6";
-	// Config variables. Consult https://www.textlocal.com/product/send//docs for more info.
-	$test = "0";
-	// Data for text message. This is the text message data.
-	$sender = "LifeStream"; // This is who the message appears to be from.
-	$numbers = "$phone_no"; // A single number or a comma-seperated list of numbers
-	$message = "Welcome to Our Website of LifeStream Blood donation  Now you are sucsessfully Registered as Official Donner.";
-	// 612 chars or less
-	// A single number or a comma-seperated list of numbers
-	$message = urlencode($message);
-	$data = "username=".$username."&hash=".$hash."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
-	$ch = curl_init('https://www.textlocal.com/product/send/');
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$result = curl_exec($ch); // This is the result from the API
-	curl_close($ch);
-?>
 	<? }
 	else{
 		?>

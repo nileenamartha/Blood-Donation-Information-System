c93ffeb9b51c3f1096128d1c5be163f76f0df5f9
Nileena Martha Vincent <10385521@mydbs.ie>
2018-08-07 11:53:47 +0100

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,6 +1,6 @@
 <?php
 $nid=$_GET["na"];
-$conn=mysqli_connect("localhost","root","","blood_info");
+$conn=mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6");
 $sql="select phone_no from notification where id='$nid'";
 $result=mysqli_query($conn,$sql);
       if(mysqli_num_rows($result)>0)

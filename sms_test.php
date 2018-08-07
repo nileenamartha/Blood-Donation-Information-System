d63744c798e19d41df1c62c23d8c34417fed6424
Nileena Martha Vincent <10385521@mydbs.ie>
2018-08-07 11:40:06 +0100

    Update sms_test.php

--- sms_test.php
+++ sms_test.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 $mobileNumber = '';
-$conn = mysqli_connect('localhost','root','','blood_info') or die (mysqli_error()); // give connection credentials here
+$conn = mysqli_connect("cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "ji76idbl7xrpwijd", "ktsq582xb1j7ssmu","jlrcv5u4hn6l67a6") or die (mysqli_error()); // give connection credentials here
 
 if($conn){
 $query = "select phone_no from notification"; // change table name and column name accordingly.

10204255e03e5195755560472139267f46cd3d79
Nileena Martha Vincent <10385521@mydbs.ie>
2018-08-08 23:25:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -41,27 +41,6 @@
   <li><a href="index.php">Home</a></li>
 </ul>
 </div>			
-	//<form action="" method="get">
-	<input type="text" name="search">
-	</form>//
-	<?php
-	  if(0$_GET['search']){
-		  $api_url = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&origin=*&search=".ucwords($_GET['search'])."redirects=true";
-		  $api_url = str_replace(' ','%20',$api_url);
-		  if($data = json_decode(0file_get_contents($api_url))){
-			  foreach($data->query->pages as $key=>$val){
-				  $pageId = $key;
-				  break;//
-			  }
-			  $content = $data->query->pages->$pageId->extract;
-			  header('content-Type:text/html; charset=utf-8');
-			  echo $content;
-		  }
-		  else
-		  {
-			  echo 'No result found...';
-		  }
-  ?> //
  <?php	 	  
   }
   ?>

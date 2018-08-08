35c3923086958352886e3d16764dec31e71f7704
Nileena Martha Vincent <10385521@mydbs.ie>
2018-08-08 10:16:47 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,43 @@
+<?php
+$nid=$_GET["na"];
+$conn=mysqli_connect("localhost","root","","blood_info");
+$sql="select phone_no from notification where id='$nid'";
+$result=mysqli_query($conn,$sql);
+      if(mysqli_num_rows($result)>0)
+      {
+          while($data=mysqli_fetch_array($result))
+          {
+        $to=$data["phone_no"];
+        ?>
+
+
+<?php
+ $username = "nileenamv@gmail.com";
+$hash = "bdbb469355e2139b95b6e545bee29c6d4027d7e19d482f0782aed18fe75350f6";
+// Config variables. Consult http://api.txtlocal.com/docs for more info.
+	$test = "0";
+
+// Data for text message. This is the text message data.
+$sender = "Lifestream"; // This is who the message appears to be from..
+  $sender = "Life Stream blood info"; // This is who the message appears to be from.
+  $numbers = ""; // A single number or a comma-seperated list of numbers
+  $message = "Welcome to Our Website LifeStream Blood donation .";
+  // 612 chars or less
+  // A single number or a comma-seperated list of numbers
+  $message = urlencode($message);
+  $data = "username=".$username."&hash=".$hash."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
+ $ch = curl_init('http://api.txtlocal.com/send/?');
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($ch); // This is the result from the API
+  curl_close($ch);
+?>
+<script>
+alert("");
+window.location="notification.php";
+</script>
+
+
+
+

741af307ced31f1f27fe2c6e3e0a54340cb83ce9
AnjithaJohnson <39301324+AnjithaJohnson@users.noreply.github.com>
2018-08-10 15:28:21 +0100

    Update action_donor_register.php

--- action_donor_register.php
+++ action_donor_register.php
@@ -43,22 +43,35 @@ else
 	$sql="INSERT INTO donor_register VALUES (null,'$lid','$name','$age','$bloodgroup','$gender','$dob','$weight','$address','$phone_no','$state','$district','$location','$status','$email')";
 	$query=mysqli_query($conn,$sql);
 	if($query){
-		?>
+	//Testing SmS	
+	require('textlocal.class.php');
+
+	$textlocal = new Textlocal(false,false, 'BIQUMilYdl4-cHeNgYIE03vFTPrTEqm1QPNFGTJMh5');
+
+	$numbers = array($phone_no);
+	$sender = 'Lifestream';
+	$message = 'Welcome to LifeStream';
+
+	try {
+	    $result = $textlocal->sendSms($numbers, $message, $sender);
+	    ?>
 		<script>
 			alert("Registration Completed.");
 			window.location.href="index.php";
 		</script>
 <?php
-		//Testing SmS
-
+	} catch (Exception $e) {
+	    //die('Error: ' . $e->getMessage());
+	}
+	/*
 	// Authorisation details.
 	$username = "nileenamv@gmail.com";
 	$hash = "bdbb469355e2139b95b6e545bee29c6d4027d7e19d482f0782aed18fe75350f6";
 	// Config variables. Consult https://www.textlocal.com/product/send//docs for more info.
 	$test = "0";
 	// Data for text message. This is the text message data.
-	$sender = "LifeStream"; // This is who the message appears to be from.
-	$numbers = "$phone_no"; // A single number or a comma-seperated list of numbers
+	$sender = "LifeStream Blood donation"; // This is who the message appears to be from.
+	$numbers = $phone_no; // A single number or a comma-seperated list of numbers
 	$message = "Welcome to Our Website of LifeStream Blood donation  Now you are sucsessfully Registered as Official Donor.";
 	// 612 chars or less
 	// A single number or a comma-seperated list of numbers
@@ -70,8 +83,8 @@ else
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$result = curl_exec($ch); // This is the result from the API
 	curl_close($ch);
-?>
-	<? }
+	*/
+	 }
 	else{
 		?>
 		<script>

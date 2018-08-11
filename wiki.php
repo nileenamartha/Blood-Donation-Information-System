b792d7816e91061b8ac8d2caeb2f32a4c97d6692
Nileena Martha Vincent <10385521@mydbs.ie>
2018-08-11 11:34:42 +0100

    Update wiki.php

--- wiki.php
+++ wiki.php
@@ -6,7 +6,7 @@
     <!DOCTYPE html>
     <html>
     <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleindex.css">
     </head>
     <body>
     <div id="navBar">
